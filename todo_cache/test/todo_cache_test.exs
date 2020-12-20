00c9780a97ff886c959fa912711f202f9fc969b7
Joaquín Benavides <joaquin.benavides@sumup.com>
2020-12-20 09:48:57 -0300

    Turn TODO into OTP application

--- todo_cache/test/todo_cache_test.exs
+++ todo_cache/test/todo_cache_test.exs
@@ -2,7 +2,6 @@ defmodule TodoCacheTest do
   use ExUnit.Case
 
   test "server_process" do
-    {:ok, cache} = Todo.Cache.start()
     bob_pid = Todo.Cache.server_process(cache, "bob")
 
     assert bob_pid != Todo.Cache.server_process(cache, "alice")
