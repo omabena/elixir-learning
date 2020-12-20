bf3098de0e3b96a8128fcf3f494939dd62f688cf
Joaquín Benavides <joaquin.benavides@sumup.com>
2020-12-20 13:27:51 -0300

    Use poolboy to manage pooling of databases workers

--- todo_cache/lib/todo/database_worker.ex
+++ todo_cache/lib/todo/database_worker.ex
@@ -1,22 +1,17 @@
 defmodule Todo.DatabaseWorker do
   use GenServer
 
-  def start_link({db_folder, worker_id}) do
-    IO.inspect("Starting database worker #{worker_id}")
-    GenServer.start_link(__MODULE__, db_folder, name: via_tuple(worker_id))
+  def start_link(db_folder) do
+    GenServer.start_link(__MODULE__, db_folder)
   end
 
   @spec store(atom | pid | {atom, any} | {:via, atom, any}, any, any) :: :ok
-  def store(worker_pid, key, data) do
-    GenServer.cast(via_tuple(worker_pid), {:store, key, data})
+  def store(pid, key, data) do
+    GenServer.cast(pid, {:store, key, data})
   end
 
-  def get(worker_pid, key) do
-    GenServer.call(via_tuple(worker_pid), {:get, key})
-  end
-
-  defp via_tuple(worker_id) do
-    Todo.ProcessRegistry.via_tuple({__MODULE__, worker_id})
+  def get(pid, key) do
+    GenServer.call(pid, {:get, key})
   end
 
   @impl true
